347b44f52115ef9d9e3afeed995f12382e0d0c5c
DarkVenusJB <Darkvenus.job@gmail.com>
2024-03-01 13:59:20 +0300

    BasicDragAndDropForCubes

--- Assets/Scripts/CubeMover.cs
+++ Assets/Scripts/CubeMover.cs
@@ -1,6 +1,35 @@
 using UnityEngine;
 
+
 public class CubeMover : MonoBehaviour
 {
-    
+    private Vector3 mousePosition;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnMouseDown()
+    {
+        mousePosition = Input.mousePosition-GetMousePosition();
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePosition);
+    }
+
+    private void OnMouseUp()
+    {
+
+    }
+
+    private Vector3 GetMousePosition()
+    {
+        return Camera.main.WorldToScreenPoint(transform.position);
+    }
+
+
 }
