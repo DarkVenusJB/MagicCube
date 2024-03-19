2ec7e47761768296807af41469144522d4c1eae5
DarkVenusJB <Darkvenus.job@gmail.com>
2024-03-19 13:18:24 +0300

    GUIPrototypeAndTimer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float startTime;
+    private TMP_Text timerTXT;
+
+    private void Start()
+    {
+        timerTXT = GetComponent<TMP_Text>();
+    }
+    private void Update()
+    {
+        startTime += Time.deltaTime;
+        int seconds = (int)(startTime % 60);
+        int minutes = (int)(startTime / 60) % 60;
+        int hours = (int)(startTime / 3600) % 24;
+
+        string timerSTR = string.Format("{0:0}:{1:00}:{2:00}", hours, minutes, seconds);
+        timerTXT.text = timerSTR;
+    }
+}
